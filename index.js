d44b33b2dd8c09e8b83d52295ba107ca1afecc1f
yousefhindi1 <yousefhindi8422@gmail.com>
2024-02-06 00:06:18 -0500

    second commit

--- index.js
+++ index.js
@@ -11,27 +11,38 @@ class Numbers {
     }
     count() {
       //return the count of numbers in data
+      return this.data.length;
     }
     printNumbers() {
       //print the numbers in data
+      this.data.forEach((number, index) => {
+        console.log(`Index: ${index}, Number: ${number}`);
+        
+      });
     }
     odds() {
       //return the odd numbers in data
+      return this.data.filter(number => number % 2 !== 0);
     }
     evens() {
       //return the even numbers in data
+      return this.data.filter(number => number % 2 === 0);
     }
     sum() {
       //return the sum of the numbers
+      return this.data.filter((acc,number) => number + acc, 0);
     }
     product() {
       //return the product of the numbers
+      return this.data.filter((acc,number) => number * acc, 1);
     }
     greaterThan(target) {
       //return the numbers greater than the target
+      return this.data.filter(number => number > target);
     }
     howMany(target) {
       //return the count of a given number
+      return this.data.filter(number => number  === target).length;
     }
   }
   
